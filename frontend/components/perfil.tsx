2fa28f8d8976ef13fbb1cb8df24c7d46e65b7ceb
alpaulaarias <paulaandreaarias307@gmail.com>
2025-08-26 17:02:02 -0500

    cerrar sesion

--- frontend/components/perfil.tsx
+++ frontend/components/perfil.tsx
@@ -1,35 +1,52 @@
-import React, { useState, useEffect } from "react";
-import { View, Text, FlatList, TouchableOpacity, StyleSheet, Alert } from "react-native";
-import { Ionicons } from "@expo/vector-icons";
+import React from "react";
+import { View, Text, TouchableOpacity, StyleSheet, Alert } from "react-native";
 import { StackParamList } from '../types/types';
 import { useNavigation } from '@react-navigation/native';
 import { NativeStackNavigationProp } from '@react-navigation/native-stack';
-import {URL} from '../config/UrlApi'
-
-
-
+import AsyncStorage from "@react-native-async-storage/async-storage";
 
 const PerfilPantalla: React.FC = () =>  {
-const navigation = useNavigation<NativeStackNavigationProp<StackParamList>>();
-    return(
-        <View style={styles.container}>
-            <Text style={styles.title}>Mi perfil</Text>
+  const navigation = useNavigation<NativeStackNavigationProp<StackParamList>>();
 
-            <TouchableOpacity>
-                <Text>
-                    Editar mi información
-                </Text>
-            </TouchableOpacity>
-            <TouchableOpacity onPress={() => navigation.navigate('PublicacionesUsuarioLogueado')}>
-                <Text>ver mis publicaciones</Text>
-            </TouchableOpacity>
-        </View>
+  //cerrar sesión
+  const handleLogout = async () => {
+    try {
+      await AsyncStorage.removeItem("userToken"); 
+      await AsyncStorage.removeItem("userData");  
 
-    )
+      Alert.alert("Sesión cerrada", "Has cerrado sesión correctamente.");
+      navigation.reset({
+        index: 0,
+        routes: [{ name: "Presentacion" }], 
+      });
+
+    } catch (error) {
+      console.error("Error al cerrar sesión:", error);
+    }
+  };
+
+  return(
+    <View style={styles.container}>
+      <Text style={styles.title}>Mi perfil</Text>
+
+      <TouchableOpacity>
+        <Text>Editar mi información</Text>
+      </TouchableOpacity>
+
+      <TouchableOpacity onPress={() => navigation.navigate('PublicacionesUsuarioLogueado')}>
+        <Text>Ver mis publicaciones</Text>
+      </TouchableOpacity>
+
+      {/* Botón cerrar sesión */}
+      <TouchableOpacity style={styles.logoutButton} onPress={handleLogout}>
+        <Text style={styles.logoutText}>Cerrar Sesión</Text>
+      </TouchableOpacity>
+    </View>
+  )
 }
 
 const styles = StyleSheet.create({
-     container: {
+  container: {
     flex: 1,
     padding: 20,
     backgroundColor: '#f8f9fa',
@@ -42,5 +59,15 @@ const styles = StyleSheet.create({
     textAlign: 'center',
     marginBottom: 30,
   },
+  logoutButton: {
+    marginTop: 40,
+    
+  },
+  logoutText: {
+    color: '#080808ff',
+    fontSize: 20,
+    
+  }
 })
-export default PerfilPantalla
+
+export default PerfilPantalla;
